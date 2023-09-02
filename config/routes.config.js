b8320b485a3b6db1d6648724d4eca19e10332761
MaineKK <133708577+MaineKK@users.noreply.github.com>
2023-09-02 11:10:24 +0200

    Update routes.config.js

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,12 +1,11 @@
 const express = require ('express');
 const router = express.Router();
 
-const user = require ('../controllers/user.controller');
+const users = require ('../controllers/user.controller');
 
 router.get('/register', users.register);
 router.post('/register', users.doRegister);
 router.get('/login', users.login);
 router.post('/login', users.doLogin);
-;
 
 module.exports = router;
