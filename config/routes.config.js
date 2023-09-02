770d929ac083505084ed92e62597d79e3348e17b
MaineKK <maralinew@gmail.com>
2023-09-02 14:35:04 +0200

    middleware added

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const users = require('../controllers/users.controller');
 const rooms = require('../controllers/rooms.controller');
+const secure = require('../middlewares/secure.mid');
 
 
 router.get('/register', users.register);
 router.post('/doRegister', users.doRegister);
 router.get('/login', users.login);
 router.post('/login', users.doLogin);
+router.get('/profile', secure.isAuthenticated, users.profile);
 
 router.get('/rooms', rooms.searchRooms)
 
