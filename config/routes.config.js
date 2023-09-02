a626410b5e6d406f287e24756205d71a645fe0a3
AlvaroGC95 <107547881+AlvaroGC95@users.noreply.github.com>
2023-09-02 10:36:22 +0200

    Update routes.config.js

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const user = require ('../controllers/user.controller');
 
-router.get ('/register', user.register);
-
-
+router.get('/register', users.register);
+router.post('/register', users.doRegister);
+router.get('/login', users.login);
+router.post('/login', users.doLogin);
+router.get('/profile', secure.isAuthenticated, users.profile);
 
 module.exports = router;
