b4ce024bab7448ca20efeaddfdf5b32d01b34aef
AlvaroGC95 <107547881+AlvaroGC95@users.noreply.github.com>
2023-09-02 11:08:06 +0200

    Update routes.config.js

--- config/routes.config.js
+++ config/routes.config.js
@@ -7,6 +7,6 @@ router.get('/register', users.register);
 router.post('/register', users.doRegister);
 router.get('/login', users.login);
 router.post('/login', users.doLogin);
-router.get('/profile', secure.isAuthenticated, users.profile);
+;
 
 module.exports = router;
