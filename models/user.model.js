040e340d1a1186bc9c8efa71409599eabb7fcdc1
AlvaroGC95 <107547881+AlvaroGC95@users.noreply.github.com>
2023-09-02 11:09:01 +0200

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,9 @@ const bcrypt = requiere ('bcryptjs');
 const WORK_FACTOR = 12;
 const Schema = mongoose.Schema;
 
+const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 const userSchema = new Schema({
     name: {
       type: String,
