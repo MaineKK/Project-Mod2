4d24de686f1d6cc1afc69727356471f5eb3e853b
AlvaroGC95 <107547881+AlvaroGC95@users.noreply.github.com>
2023-09-02 10:05:15 +0200

    Update routes.config.js

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,1 +1,10 @@
+const express = require ('express');
+const router = express.Router();
 
+const user = require ('../controllers/user.controller');
+
+router.get ('/register', user.register);
+
+
+
+module.exports = router;
